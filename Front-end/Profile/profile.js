7bca16a1e5125d33eed1597f20f6cf3cca77d4da
Bruno Januário <januario@MacBook-Air-de-Bruno.local>
2021-12-09 23:09:12 +0000

    profile with jQuery

--- Front-end/Profile/profile.js
+++ Front-end/Profile/profile.js
@@ -1,28 +1,37 @@
 $(document).ready(function () {
-    var giver = {
-        name: "Bruno Januário",
-        phoneNumber: 911776058,
-        email: "bruno.januario1998@gmail.com",
-        aboutMe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        imgUrl: "./photo.jpeg",
-        category: "Sport",
-        skills: "Soccer",
-        experiences: "Coach",
-        location: "Porto",
-        age: 23
+    var giver;
+
+
+
+    function successCallback(response) {
+
+        giver = response;
+
+        $("#name").append(giver.name);
+        $("#phoneNumber").append(giver.phoneNumber);
+        $("#email").append(giver.email);
+        $("#aboutMe").append(giver.aboutMe);
+        $("#category").append(giver.category);
+        $("#skills").append(giver.skills);
+        $("#experiences").append(giver.experiences);
+        $("#location").append(giver.location);
+        $("#age").append(giver.age);
+
+        var src1 = giver.imgUrl;
+        $("#photo").attr("src", src1);
     }
 
-    $("#name").append(giver.name);
-    $("#phoneNumber").append(giver.phoneNumber);
-    $("#email").append(giver.email);
-    $("#aboutMe").append(giver.aboutMe);
-    $("#category").append(giver.category);
-    $("#skills").append(giver.skills);
-    $("#experiences").append(giver.experiences);
-    $("#location").append(giver.location);
-    $("#age").append(giver.age);
+    function errorCallback(request, status, error) {
+        console.log(error);
+    }
 
-    var src1 = 'photo.jpeg';
-    $("#photo").attr("src", src1);
+    // perform an ajax http get request
+    $.ajax({
+        url: 'http://192.168.1.14:8080/api/1',
+        crossOrigin: true,
+        async: true,
+        success: successCallback,
+        error: errorCallback
+    });
 
 });
