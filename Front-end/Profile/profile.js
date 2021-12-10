da04c83cbc80625c655d259a4bd14337c3747d82
Bruno Januário <januario@MacBook-Air-de-Bruno.local>
2021-12-10 02:46:53 +0000

    profile done

--- Front-end/Profile/profile.js
+++ Front-end/Profile/profile.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     var giver;
-
-
+    var url = window.location.pathname;
+    var idStr = url.split("/");
+    var id = parseInt(idStr[idStr.length - 1]);
 
     function successCallback(response) {
 
@@ -27,7 +28,7 @@ $(document).ready(function () {
 
     // perform an ajax http get request
     $.ajax({
-        url: 'http://192.168.1.14:8080/api/1',
+        url: 'http://192.168.1.14:8080/api/' + id,
         crossOrigin: true,
         async: true,
         success: successCallback,
